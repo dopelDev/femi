2642af8134de94a4bacd693246dbd941669367b4
Dopel <322kuroneko@gmail.com>
2022-03-25 06:30:20 -0500

    add 2 new graph

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html', totales=get_data.push_data_totales())
+    return render_template('index.html', totales=get_data.push_data_totales(), totales_line=get_data.push_data_totales_line())
 
 if __name__ == '__main__':
     app.run()
--- methods/get_data.py
+++ methods/get_data.py
@@ -9,8 +9,61 @@ def push_data_totales():
     values = [value[0] for value in data.iloc[:,1:2].values]
     labels = []
     for item in labels_tmp:
-        labels.append(str(item))
+        labels.append(item)
     
-    print(labels)
     data_delivery = {'labels': labels, 'values' : values}
     return data_delivery
+
+def push_data_totales_line():
+    data = pd.read_csv('static/data/clean_data.csv', index_col='VÍCTIMAS DE FEMINICIDIO, SEGÚN DEPARTAMENTO, 2015 - 2019')
+    labels = data.iloc[:1, 2:].values.tolist()[0]
+    values = data.iloc[1:2,2:].values.tolist()[0]
+    
+    data_delivery = {'labels': labels, 'values' : values}
+    return data_delivery
+
+def push_data_pie_totales():
+    data = pd.read_csv('static/data/totales_por_departamento.csv')
+    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
+    values = [value[0] for value in data.iloc[:,1:2].values]
+    labels = []
+    for item in labels_tmp:
+        labels.append(item)
+    
+    data_delivery = {'labels': labels, 'values' : values}
+    return data_delivery
+
+def push_data_cusco_la_libertad():
+    data = pd.read_csv('static/data/cusco_la_libertad.csv')
+    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
+    values = [value[0] for value in data.iloc[:,1:2].values]
+    labels = []
+    for item in labels_tmp:
+        labels.append(item)
+    
+    data_delivery = {'labels': labels, 'values' : values}
+    return data_delivery
+
+def push_data_top_5_2018():
+    data = pd.read_csv('static/data/sorting_2018.csv')
+    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
+    values = [value[0] for value in data.iloc[:,1:2].values]
+    labels = []
+    for item in labels_tmp:
+        labels.append(item)
+    
+    data_delivery = {'labels': labels, 'values' : values}
+    return data_delivery
+
+def push_data_top_10_totales():
+    data = pd.read_csv('static/data/totales_por_departamento.csv')
+    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
+    values = [value[0] for value in data.iloc[:,1:2].values]
+    labels = []
+    for item in labels_tmp:
+        labels.append(item)
+    
+    data_delivery = {'labels': labels, 'values' : values}
+    return data_delivery
+
+
