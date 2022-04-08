fc9180efde7e8cb257029fabac107e50e887b936
dopelDev <322kuroneko@gmail.com>
2022-04-08 03:00:12 -0500

    hecho todas las imagines

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html', totales=get_data.push_data_totales(), totales_line=get_data.push_data_totales_line())
+    return render_template('index.html', totales=get_data.push_data_totales(), totales_line=get_data.push_data_totales_line(), top_5_2018=get_data.push_data_top_5_2018(), top_10_totales=get_data.push_data_top_10_totales(), cusco_la_libertad=get_data.push_data_cusco_la_libertad())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- methods/get_data.py
+++ methods/get_data.py
@@ -22,37 +22,33 @@ def push_data_totales_line():
     data_delivery = {'labels': labels, 'values' : values}
     return data_delivery
 
-def push_data_cusco_la_libertad():
-    data = pd.read_csv('static/data/cusco_la_libertad.csv')
-    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
-    values = [value[0] for value in data.iloc[:,1:2].values]
-    labels = []
-    for item in labels_tmp:
-        labels.append(item)
-    
-    data_delivery = {'labels': labels, 'values' : values}
-    return data_delivery
-
 def push_data_top_5_2018():
     data = pd.read_csv('static/data/sorting_2018.csv')
-    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
-    values = [value[0] for value in data.iloc[:,1:2].values]
-    labels = []
-    for item in labels_tmp:
-        labels.append(item)
+    labels_tmp = data.iloc[:5,:1].values.tolist()
+    values_tmp = data.iloc[:5,1:].values.tolist()
     
+    labels = [label[0] for label in labels_tmp]
+    values = [value[0] for value in values_tmp]
+
     data_delivery = {'labels': labels, 'values' : values}
     return data_delivery
 
 def push_data_top_10_totales():
     data = pd.read_csv('static/data/totales_por_departamento.csv')
-    labels_tmp = [value[0] for value in data.iloc[:,:1].values]
-    values = [value[0] for value in data.iloc[:,1:2].values]
-    labels = []
-    for item in labels_tmp:
-        labels.append(item)
-    
+    labels = [label[0] for label in data.iloc[:10,:1].values]
+    values = [value[0] for value in data.iloc[:10,1:].values]
+   
     data_delivery = {'labels': labels, 'values' : values}
     return data_delivery
 
+def push_data_cusco_la_libertad():
+    data = pd.read_csv('static/data/cusco_la_libertad.csv')
+    labels = [label for label in data.T.index.values]
+    # data la libertad
+    values_la_libertad = [value[0] for value in data.T.iloc[2:,:1].values]
+    # data cusco
+    values_cusco = [value[0] for value in data.T.iloc[2:,1:].values]
+
+    data_delivery = {'labels': labels[2:], 'values' : {'la libertad' : values_la_libertad, 'cusco' : values_cusco}}
+    return data_delivery
 
