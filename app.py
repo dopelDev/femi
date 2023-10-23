bdd924f45160554c914c8c2c277c377dc8e82f88
dopeldev <322kuroneko@gmail.com>
2023-10-23 02:55:55 -0500

    requirements.txt updated

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return render_template('index.html', totales=get_data.push_data_totales(), totales_line=get_data.push_data_totales_line(), top_5_2018=get_data.push_data_top_5_2018(), top_10_totales=get_data.push_data_top_10_totales(), cusco_la_libertad=get_data.push_data_cusco_la_libertad())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
